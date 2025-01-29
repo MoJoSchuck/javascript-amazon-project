0060e9783cecc54782982a372a05e6ca8934d112
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2025-01-29 13:49:45 +0100

    Make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calculateCartQuantity, updateQuantity } from '../data/cart.js';
+import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -105,11 +105,10 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
             ? 'FREE'
             : `$${formatCurrency(deliveryOption.priceCents)} -`;
 
-        const isChecked = deliveryOption.id === 
-        cartItem.deliveryOptionId;
+        const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
         html +=  `
-            <div class="delivery-option">
+            <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id=${deliveryOption.id}>
                 <input type="radio" ${isChecked ? 'checked' : ''} class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
                 <div>
                     <div class="delivery-option-date"> 
@@ -195,3 +194,16 @@ function updateCartQuantity() {
 }
 
 updateCartQuantity();
+
+
+document.querySelectorAll('.js-delivery-option')
+    .forEach((element) => {
+        element.addEventListener('click', () => {
+            const {productId, deliveryOptionId} = element.dataset;
+            // Shorthand Property
+            // const productId = element.dataset.productId;
+            // const deliveryOptionId = element.dataset.deliveryOptionId;
+
+            updateDeliveryOption(productId, deliveryOptionId);
+        })
+    });
