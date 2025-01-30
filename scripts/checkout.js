e5061dd81485bf13aa3b1e6720b01f1ef8ff7fa6
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2025-01-30 17:39:05 +0100

    Save data and generate HTML for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,5 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
+import { renderPaymentSummary } from './checkout/payment.Summary.js';
 
 renderOrderSummary();
+renderPaymentSummary();
