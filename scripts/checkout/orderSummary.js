afd9205df357ac3cba1f784a0af5e01ea8fa61eb
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2025-02-05 14:43:01 +0100

    Update

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,7 +4,7 @@ import { formatCurrency } from '../utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption, calculateDeliveryDate } from '../../data/deliveryOptions.js';
-import { renderPaymentSummary } from './payment.Summary.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 import { renderCheckoutHeader } from './checkoutHeader.js';
 
 export function renderOrderSummary() {
