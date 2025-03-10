41e9715344c3bcc9485575348e00a858c1ac7813
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2025-03-10 15:41:22 +0100

    Enhance search functionality: filter products by keywords and make search case-insensitive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -14,7 +14,16 @@ function renderProductsGrid() {
 
     if (search) {
         filterdProducts = products.filter((product) => {
-            return product.name.includes(search);
+            let matchingKeyword = false;
+
+            product.keywords.forEach((keyword) => {
+                if (keyword.toLowerCase().includes(search.toLowerCase())) {
+                    matchingKeyword = true;
+                }
+            });
+
+            return matchingKeyword ||
+                product.name.toLowerCase().includes(search.toLowerCase());
         });
     }
 
